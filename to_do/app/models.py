5356739e97d750ebc6f95a9c0bf72ba7e30eefd1
YoussefIbraheem <youssefm.muhammed@gmail.com>
2025-09-02 00:47:48 +0300

    Add ToDo and Category models with migrations
    - Implemented ToDo model with title, description, status, user, and categories fields.
    - Created Category model with a name field.
    - Added ManyToMany relationship between ToDo and Category models.

--- to_do/app/models.py
+++ to_do/app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+    
+
+class ToDo(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "pending" , "Pending"
+        IN_PROGRESS = "in_progress" , "In Progress"
+        COMPLETED = "completed" , "Completed"
+        CANCELLED = "cancelled" , "Cancelled"
+    
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000)
+    status = models.CharField(choices=StatusChoices,default=StatusChoices.PENDING)
+    
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    
+    def __str__(self):
+        return self.title
+    
