077e31c0e7deee8041e013d4a8a6024b4464e243
YoussefIbraheem <youssefm.muhammed@gmail.com>
2025-09-03 00:32:32 +0300

    Register ToDo and Category models in the Django admin with custom admin classes

--- to_do/app/admin.py
+++ to_do/app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import ToDo, Category
 
-# Register your models here.
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status')
+    search_fields = ('title',)
+    ordering = ('title',)
+    
+    
+    
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(ToDo, ToDoAdmin)
